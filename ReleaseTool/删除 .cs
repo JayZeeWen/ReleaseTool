4dbfa9391a33816cac22275a20df60465c7428d9
562230790@qq.com <562230790@qq.com>
2015-12-31 10:11:15 +0800

    First
    First

---
+++ ReleaseTool/删除 .cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace testWinForm
+{
+    public partial class 删除 : Form
+    {
+        #region Pro/属性
+        protected string tPath
+        {
+            get
+            {
+                return txtTargetPath.Text.Trim();
+            }
+        }
+
+        protected string bPath
+        {
+            get
+            {
+                return txtBackUpPath.Text.Trim();
+            }
+        }
+
+        protected string sPath
+        {
+            get
+            {
+                return txtPath.Text.Trim();
+            }
+        }
+        #endregion 
+
+        public 删除()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDel_Click(object sender, EventArgs e)
+        {
+            string path = txtPath.Text.ToString().Trim();
+            string txtDate = txtLastDate.Text.ToString().Trim();
+            DateTime date = Convert.ToDateTime(txtDate);
+            DeleteFileWithPostfix(path, "*.config");
+            DeleteFile(path, date);
+            MessageBox.Show( "删除完成");
+        }
+
+        private void btnBackUp_Click(object sender, EventArgs e)
+        {
+            string targetPath = txtTargetPath.Text.Trim();
+            string backupPath = txtBackUpPath.Text.Trim();
+            string sourcePath = txtPath.Text.Trim();
+            BackUpFile(targetPath, backupPath, sourcePath);
+            
+        }
+
+        /// <summary>
+        /// 删除日期之前的文件
+        /// </summary>
+        /// <param name="path">路径</param>
+        /// <param name="date">最后修改日期</param>
+        public void DeleteFile(string path, DateTime date)
+        {      
+            DirectoryInfo dir = new DirectoryInfo(path);
+            DirectoryInfo[] dirs = null;
+            FileInfo[] fileList = null;
+            if (dir.Exists)
+            {
+                fileList = dir.GetFiles();
+                foreach (FileInfo file in fileList)
+                {
+                    if (file.LastWriteTime < date)
+                    {
+                        file.Delete();
+                    }
+                }
+
+                dirs = dir.GetDirectories();
+                foreach (DirectoryInfo info in dirs)
+                {
+                    DeleteFile(info.FullName, date);
+                    if (info.GetFiles().Length == 0  && info.GetDirectories().Length == 0)
+                    {
+                        Directory.Delete(info.FullName);
+                    }
+                }
+            }
+            
+        }
+
+        //删除指定后缀名的文件
+        public void DeleteFileWithPostfix(string path , string postfixName)
+        {
+            DirectoryInfo dir = new DirectoryInfo(path);
+            if (dir.Exists)
+            {                
+                foreach (FileInfo file in dir.GetFiles( postfixName))
+                {
+                    if (file.Attributes.ToString().IndexOf("ReadOnly") != -1)
+                    {
+                        file.Attributes = FileAttributes.Normal;
+                    }
+                    file.Delete();
+                }
+            }
+        }
+
+
+        public void BackUpFile(string targetPath, string backupPath, string sourcePath)
+        {
+            DirectoryInfo dir = new DirectoryInfo(sourcePath);
+            DirectoryInfo targetDir = new DirectoryInfo(targetPath);
+            DirectoryInfo[] dirs = null;
+            FileInfo[] files = null;
+            if (dir.Exists)
+            {
+                
+                files = dir.GetFiles();
+                foreach(FileInfo file in files)
+                {
+                    string name = file.FullName;
+
+                }
+                dirs = dir.GetDirectories();
+
+            }
+        }
+
+        
+
+        
+
+    }
+}
---
+++ ReleaseTool/删除 .designer.cs
@@ -0,0 +1,166 @@
+﻿namespace testWinForm
+{
+    partial class 删除
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtLastDate = new System.Windows.Forms.TextBox();
+            this.btnDel = new System.Windows.Forms.Button();
+            this.btnBackUp = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtTargetPath = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtBackUpPath = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(113, 64);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(162, 21);
+            this.txtPath.TabIndex = 0;
+            this.txtPath.Text = "E:\\test";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(66, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "源路径";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 154);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "最后修改时间";
+            // 
+            // txtLastDate
+            // 
+            this.txtLastDate.Location = new System.Drawing.Point(113, 151);
+            this.txtLastDate.Name = "txtLastDate";
+            this.txtLastDate.Size = new System.Drawing.Size(162, 21);
+            this.txtLastDate.TabIndex = 2;
+            this.txtLastDate.Text = "2015-12-15 16:28";
+            // 
+            // btnDel
+            // 
+            this.btnDel.Location = new System.Drawing.Point(68, 190);
+            this.btnDel.Name = "btnDel";
+            this.btnDel.Size = new System.Drawing.Size(75, 23);
+            this.btnDel.TabIndex = 4;
+            this.btnDel.Text = "删除";
+            this.btnDel.UseVisualStyleBackColor = true;
+            this.btnDel.Click += new System.EventHandler(this.btnDel_Click);
+            // 
+            // btnBackUp
+            // 
+            this.btnBackUp.Location = new System.Drawing.Point(200, 190);
+            this.btnBackUp.Name = "btnBackUp";
+            this.btnBackUp.Size = new System.Drawing.Size(75, 23);
+            this.btnBackUp.TabIndex = 5;
+            this.btnBackUp.Text = "备份";
+            this.btnBackUp.UseVisualStyleBackColor = true;
+            this.btnBackUp.Click += new System.EventHandler(this.btnBackUp_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(54, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "目标路径";
+            // 
+            // txtTargetPath
+            // 
+            this.txtTargetPath.Location = new System.Drawing.Point(113, 91);
+            this.txtTargetPath.Name = "txtTargetPath";
+            this.txtTargetPath.Size = new System.Drawing.Size(162, 21);
+            this.txtTargetPath.TabIndex = 6;
+            this.txtTargetPath.Text = "E:\\test\\backup";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(54, 121);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 12);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "备份路径";
+            // 
+            // txtBackUpPath
+            // 
+            this.txtBackUpPath.Location = new System.Drawing.Point(113, 118);
+            this.txtBackUpPath.Name = "txtBackUpPath";
+            this.txtBackUpPath.Size = new System.Drawing.Size(162, 21);
+            this.txtBackUpPath.TabIndex = 8;
+            this.txtBackUpPath.Text = "E:\\edit";
+            // 
+            // 删除
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(404, 262);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtBackUpPath);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtTargetPath);
+            this.Controls.Add(this.btnBackUp);
+            this.Controls.Add(this.btnDel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtLastDate);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPath);
+            this.Name = "删除";
+            this.Text = "删除";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtLastDate;
+        private System.Windows.Forms.Button btnDel;
+        private System.Windows.Forms.Button btnBackUp;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtTargetPath;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtBackUpPath;
+    }
+}
